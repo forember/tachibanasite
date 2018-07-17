52f6f01f2fa06084cee98dbb5a7a3a9933763873
Chris McKinney <cmdaltpageup@gmail.com>
2018-07-17 01:29:18 -0700

    1.8.8 - updated katex and fixed SSL. Still need to update changelog

--- utils/ForceSSL.php
+++ utils/ForceSSL.php
@@ -37,7 +37,8 @@ function forceSSL($use_sts = true) {
     // iis sets HTTPS to 'off' for non-SSL requests
     if ($use_sts && isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] != 'off') {
         header('Strict-Transport-Security: max-age=31536000');
-    } elseif ($use_sts) {
+    } elseif (!isset($_SERVER['HTTPS']) || $_SERVER['HTTPS'] == ''
+            || $_SERVER['HTTPS'] == 'off') {
         header('Location: https://' . $_SERVER['HTTP_HOST']
             . $_SERVER['REQUEST_URI'], true, 301);
         // we are in cleartext at the moment, prevent further execution/output
